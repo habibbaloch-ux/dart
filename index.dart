f76c177e8f7b7085ee3b53575efcb095c26d648b
habibbaloch-ux <habibst480@gmail.com>
2025-03-12 16:45:04 +0500

    Add files via upload

---
+++ index.dart
@@ -0,0 +1,17 @@
+void main() { 
+var myfunc = (String a) {
+  return a;
+ };
+ Function show = (String a) {
+   print(a);
+ return a; 
+ }; 
+
+
+ int sum(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  print(sum(3, 4)); 
+}
